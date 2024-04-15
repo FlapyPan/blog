cef0e3f26c385cf81b6ce88b930a017013c99ae8
腾包子 <flapypan@gmail.com>
2024-04-16 00:30:45 +0800

    更新背景色

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,12 +1,3 @@
 import { defineTheme } from 'pinceau'
 
-export default defineTheme({
-  alpine: {
-    body: {
-      backgroundColor: {
-        initial: '#f8fafc',
-        dark: '#0f172a',
-      },
-    },
-  },
-})
+export default defineTheme({})
